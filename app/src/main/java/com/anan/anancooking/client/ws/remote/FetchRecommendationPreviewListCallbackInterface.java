759a89571630f573d8ad8525f4af94304d830b38
Xin <kuoxinkuoxin@gmail.com>
2015-05-01 16:29:34 -0400

    solve

---
+++ app/src/main/java/com/anan/anancooking/client/ws/remote/FetchRecommendationPreviewListCallbackInterface.java
@@ -0,0 +1,8 @@
+package com.anan.anancooking.client.ws.remote;
+
+/**
+ * Created by kuoxin on 5/1/15.
+ */
+public interface FetchRecommendationPreviewListCallbackInterface {
+    void setRecommendationPreviewList();
+}
