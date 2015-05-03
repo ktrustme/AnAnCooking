b2c1f1d824003cff53e089970087e5a8bd055b2d
zihsiangsyu <zsyu@andrew.cmu.edu>
2015-05-02 21:31:43 -0400

    commit from Siang 20150501 1645

--- app/src/main/java/com/anan/anancooking/client/ws/remote/AnAnNetworkProtocols.java
+++ app/src/main/java/com/anan/anancooking/client/ws/remote/AnAnNetworkProtocols.java
@@ -4,7 +4,7 @@ package com.anan.anancooking.client.ws.remote;
  * Created by kuoxin on 4/30/15.
  */
 public class AnAnNetworkProtocols {
-    public static String HOST_NAME="zsyuMBP.wv.cc.cmu.edu";
+    public static String HOST_NAME="kuoxindeMBP.wv.cc.cmu.edu";
     public static int PORT_NUM=8080;
     public static String WEB_APP_NAME="/AnAnCooking";
     public static String URL_PATTERN_FETCH_RECIPE="/fetch_recipe";
